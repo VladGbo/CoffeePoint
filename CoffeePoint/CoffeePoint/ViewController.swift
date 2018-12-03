89627831451f62fee13106c01ce816977f2e3570
Vladyslav Horbenko <>
2018-12-04 00:53:27 +0200

    First commit

---
+++ CoffeePoint/CoffeePoint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoffeePoint
+//
+//  Created by Vladyslav Horbenko on 12/4/18.
+//  Copyright © 2018 Vladyslav Horbenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
