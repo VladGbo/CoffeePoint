15a132960c26e5a4fab565089a94433ebea7976b
Vladyslav Horbenko <>
2018-12-04 02:21:26 +0200

    #connect firebase sdk

---
+++ CoffeePoint/CoffeePoint/Firebase/VHFirCoffeePointServices.swift
@@ -0,0 +1,16 @@
+//
+//  VHFirCoffeePointServices.swift
+//  CoffeePoint
+//
+//  Created by Vladyslav Horbenko on 12/4/18.
+//  Copyright © 2018 Vladyslav Horbenko. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class VHFirCoffeePointServices: NSObject {
+    
+    
+
+}
